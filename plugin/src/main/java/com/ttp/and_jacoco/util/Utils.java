d7b8c85d07b4a7bb78fc4d8dec2565b254935fc2
xuechaofei <felix.xue@hesung.com>
2025-05-07 17:54:37 +0800

    feat:临时移除plugin模块

--- plugin/src/main/java/com/ttp/and_jacoco/util/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.ttp.and_jacoco.util;
-
-public class Utils {
-    
-    public static boolean isWindows() {
-        return System.getProperties().getProperty("os.name").toLowerCase().contains("windows");
-    }
-}
