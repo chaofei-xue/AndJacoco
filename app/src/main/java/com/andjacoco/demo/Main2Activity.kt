17b8655fa8ebb832f5751903aed7f0159f2eaab7
xuechaofei <felix.xue@hesung.com>
2025-05-09 15:48:15 +0800

    feat:test

---
+++ app/src/main/java/com/andjacoco/demo/Main2Activity.kt
@@ -0,0 +1,34 @@
+package com.andjacoco.demo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import com.andjacoco.demo.databinding.ActivityMainBinding
+
+class Main2Activity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Hello.Toast(this, "hello132")
+
+
+        binding.tv.setOnClickListener {
+            startActivity(Intent(this, SecondActivity::class.java))
+
+            Toast.makeText(this, "Test Info", Toast.LENGTH_LONG).show()
+
+            Toast.makeText(this, "Test Code1", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Test Code2", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Test Code3", Toast.LENGTH_LONG).show()
+        }
+
+        Hello.hello(false)
+
+    }
+
+
+}
